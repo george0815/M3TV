ea85f74d1f928badb896bcee90979ad3c3ecfaa3
George S <shipmang8@gmail.com>
2022-07-30 20:31:58 -0400

    added jquery load stuff

---
+++ scripts/redirect.js
@@ -0,0 +1,158 @@
+
+
+//INDEX BUTTONS
+document.getElementById("iPlay").addEventListener("click", function() {
+
+    var idString = document.body.firstChild.id;
+
+    $("#" + idString).load("../play/play.html",function(){
+        document.body.firstChild.id = "body";
+        document.documentElement.id = ""
+    });
+
+  });
+
+
+  
+
+
+document.getElementById("iAdd").addEventListener("click", function() {
+
+    var idString = document.body.firstChild.id;
+
+    $("#" + idString).load("../add/add.html",function(){
+
+        document.body.firstChild.id = "body";
+        document.documentElement.id = ""
+
+
+
+
+    
+        //ADD BUTTONS
+        document.getElementById("options").addEventListener("click", function() {
+
+
+            var idString = document.body.firstChild.id;
+        
+            $("#" + idString).load("../options/options.html",function(){
+                document.body.firstChild.id = "";
+                document.documentElement.id = "htmlSettings"
+            });
+        
+        
+        
+          }); 
+
+
+
+          document.getElementById("aBack").addEventListener("click", function() {
+
+
+            var idString = document.body.firstChild.id;
+        
+            $("#" + idString).load("../base.html",function(){
+                document.body.firstChild.id = "MainContainer";
+                document.documentElement.id = ""
+            });
+        
+        
+        
+          }); 
+
+
+        document.getElementById("aPlaylist").addEventListener("click", function() {
+
+            var idString = document.body.firstChild.id;
+        
+            $("#" + idString).load("../add/addPlaylist.html",function(){
+                document.body.firstChild.id = "";
+                document.documentElement.id = "htmlSettings"
+            });
+        
+        });
+
+
+        document.getElementById("aChannel").addEventListener("click", function() {
+
+            var idString = document.body.firstChild.id;
+
+            $("#" + idString).load("../add/addChannel.html",function(){
+            document.body.firstChild.id = "";
+            document.documentElement.id = "htmlSettings"
+
+
+        });
+
+
+          
+        
+        
+        
+
+
+
+
+
+    });
+
+
+
+
+
+    });
+
+  });
+
+
+
+
+
+
+
+  //options
+  document.getElementById("options").addEventListener("click", function() {
+
+
+    var idString = document.body.firstChild.id;
+
+    $("#" + idString).load("../options/options.html",function(){
+        document.body.firstChild.id = "";
+        document.documentElement.id = "htmlSettings"
+    });
+
+
+
+  }); 
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PLAY BUTTONS
+
+
