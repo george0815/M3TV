d48bda7aa71f01a6a36b2f894335b7396989008e
George S <shipmang8@gmail.com>
2022-07-30 21:09:06 -0400

    stuff

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -8,6 +8,66 @@ document.getElementById("iPlay").addEventListener("click", function() {
     $("#" + idString).load("../play/play.html",function(){
         document.body.firstChild.id = "body";
         document.documentElement.id = ""
+
+
+      //ADD BUTTONS
+      document.getElementById("options").addEventListener("click", function() {
+
+
+        var idString = document.body.firstChild.id;
+    
+        $("#" + idString).load("../options/options.html",function(){
+            document.body.firstChild.id = "";
+            document.documentElement.id = "htmlSettings"
+        });
+    
+    
+    
+      }); 
+
+
+
+      document.getElementById("pBack").addEventListener("click", function() {
+
+
+        var idString = document.body.firstChild.id;
+    
+        $("#" + idString).load("../base.html",function(){
+            document.body.firstChild.id = "MainContainer";
+            document.documentElement.id = ""
+        });
+    
+    
+    
+      }); 
+
+
+    document.getElementById("pPlaylist").addEventListener("click", function() {
+
+        var idString = document.body.firstChild.id;
+    
+        $("#" + idString).load("../play/playPlaylist.html",function(){
+            document.body.firstChild.id = "";
+            document.body.firstChild.className = "playVid";
+            document.documentElement.id = ""
+        });
+    
+    });
+
+    document.getElementById("pChannel").addEventListener("click", function() {
+
+      var idString = document.body.firstChild.id;
+  
+      $("#" + idString).load("../play/playChannel.html",function(){
+          document.body.firstChild.id = "";
+          document.body.firstChild.className = "playVid";
+          document.documentElement.id = ""
+      });
+  
+  });
+
+
+        
     });
 
   });
