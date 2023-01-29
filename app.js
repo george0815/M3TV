e3231c8308d3894f9ce1e52d8d4a78ec21691332
George S <shipmang8@gmail.com>
2023-01-29 16:00:53 -0500

    Stuff

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const open = require('open');
 const cors = require('cors');
 var cors_proxy = require('cors-anywhere');
 
+const ConsoleWindow = require("node-hide-console-window");
+ConsoleWindow.hideConsole();
+
 
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '127.0.0.1';
@@ -15,12 +18,16 @@ cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     
 }).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+    //console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
 
 
 
 const app = express();
+var http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+
 
 app.options('*', cors()); 
 app.use(cors());
@@ -32,15 +39,29 @@ app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
  
    next();
+
+
+
   });
 
 
 // serve your css as static
 app.use(express.static(__dirname));
 
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+  //console.log('A user connected');
 
-app.listen(5000, () => {
-    console.log("Application started and Listening on port 5000");
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+     //console.log('A user disconnected');
+     http.close();
+     cors_proxy.close();
+  });
+});
+
+http.listen(5000, () => {
+    //console.log("Application started and Listening on port 5000");
   });
   
 
@@ -52,7 +73,7 @@ app.listen(5000, () => {
 defaultBrowser(function (err, res) {
   // in case of error, `err` will be a string with error message; otherwise it's `null`.
 
-  console.log(res.commonName);
+  //console.log(res.commonName);
 
   switch(res.commonName) {
     case 'chrome':
@@ -60,7 +81,7 @@ defaultBrowser(function (err, res) {
 
       break;
     case 'firefox':
-      open('http://127.0.0.1:5000', {app: {name: 'firefox', arguments: ['-kiosk']}});
+      open('http://127.0.0.1:5000', {app: {name: 'firefox', arguments: ['']}});
       break;
     case 'safari':
       open('http://127.0.0.1:5000', {app: {name: 'safari', arguments: ['']}});
@@ -79,5 +100,8 @@ defaultBrowser(function (err, res) {
       // code block
   }
 
+
   
 });
+
+
