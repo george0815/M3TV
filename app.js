b15951fdb749c7c7250b60b343270bb33460c271
George S <shipmang8@gmail.com>
2023-04-21 18:36:14 -0400

    updated console.log

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ var fileToRead = 'le.txt';
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8000;
+var port = process.env.PORT;//|| 8000;
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     
-}).listen(8080, '0.0.0.0', function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + '8080');
+}).listen(8000, '0.0.0.0', function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + 8000);
 });
 
 
@@ -71,7 +71,7 @@ io.on('connection', function(socket) {
 });*/
 
 http.listen(port, '0.0.0.0', function() {
-    //console.log("Application started and Listening on port 5000");
+    console.log("Application started and Listening on port:" + port);
   });
   
 
