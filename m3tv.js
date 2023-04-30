3d3d5d76ee00854aabc67b85c94e48c563982324
George S <shipmang8@gmail.com>
2023-04-30 11:42:33 -0400

    same changes as the ones in main

--- m3tv.js
+++ m3tv.js
@@ -1,17 +1,33 @@
 //Requires
+var defaultBrowser = require("x-default-browser");
 const express = require("express");
+const open = require('open');
 const cors = require('cors');
+var cors_proxy = require('cors-anywhere');
 
-//creates server  
+
+const ConsoleWindow = require("node-hide-console-window");
+ConsoleWindow.hideConsole();
+
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '127.0.0.1';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8000;
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
+
+
 const app = express();
 var http = require('http').createServer(app);
 
-
-var port = process.env.PORT || 8000;
-var host = process.env.HOST || '0.0.0.0';
-
-
-
+                                                          
 app.options('*', cors()); 
 app.use(cors());
 
@@ -33,12 +49,46 @@ app.use(express.static(__dirname));
 
 
 
-http.listen(port, host, function() {
-    console.log("Application started and Listening on host:" + host + " port:" + port);
+http.listen(5000, () => {
+    console.log("Application started and Listening on port 5000, navigate to 'http://127.0.0.1:5000' on your browser.");
   });
   
 
 
+
+// opens the url in the default browser 
+
+
+defaultBrowser(function (err, res) {
+  // in case of error, `err` will be a string with error message; otherwise it's `null`.
+  switch(res.commonName) {
+    case 'chrome':
+      open('http://127.0.0.1:5000', {app: {name: 'chrome', arguments: ['--disable-web-security', '--start-fullscreen','--user-data-dir']}});
+
+      break;
+    case 'firefox':
+      open('http://127.0.0.1:5000', {app: {name: 'firefox', arguments: ['']}});
+      break;
+    case 'safari':
+      open('http://127.0.0.1:5000', {app: {name: 'safari', arguments: ['']}});
+    break;
+    case 'edge':
+      open('http://127.0.0.1:5000', {app: {name: 'microsoftedge', arguments: ['--disable-web-security', '--start-fullscreen','--user-data-dir']}});
+    break;
+    case 'chromium':
+      open('http://127.0.0.1:5000', {app: {name: 'chromium', arguments: ['--disable-web-security', '--start-fullscreen','--user-data-dir']}});
+    break;
+    case 'opera':
+      open('http://127.0.0.1:5000', {app: {name: 'opera', arguments: ['--disable-web-security', '--start-fullscreen','--user-data-dir']}});
+    break;
+    
+    default:
+      // code block
+  }
+
+
+  
+});
 
 //Thank you for being interested in my project!
 
--- scripts/playChannel.js
+++ scripts/playChannel.js
@@ -1,198 +1,141 @@
-//player for playing vids
+// player for playing vids
 var player = document.getElementById('curPlaying');
 
-
-//used for page functionality
+// used for page functionality
 var page = 0;
 var pgCounter = 0;
 
-
-//holds index of current channel playing, and whether or not something is playing
+// holds index of current channel playing, and whether or not something is playing
 var curIndex = 0;
 var playing = false;
 
-
-
-
-//declares ichannel array
+// declares ichannel array
 var iChannels = [];
 
+// makes video element invisible
+document.getElementById('curPlaying').style.display = 'none';
 
-
-//makes video element invisible
-document.getElementById('curPlaying').style.display='none';
-
-
-
-//plays channel
-function playC(url, index){
-
-  //plays sound effect and mutes background music if it isn't already muted
+// plays channel
+function playC(url, index) {
+  // plays sound effect and mutes background music if it isn't already muted
   okChannel2.play();
-  if(!(document.getElementById("bgMusic").src.includes("&mute=1"))){
+  if (!(document.getElementById("bgMusic").src.includes("&mute=1"))) {
     document.getElementById("bgMusic").src = document.getElementById("bgMusic").src + "&mute=1";
   }
-  
 
-
-  //Remove after debugging
+  // Remove after debugging
   console.log(index);
 
-  //sets current index
+  // sets current index
   curIndex = index;
 
-  //for cors pretected channels
+  // for cors pretected channels
   const proxy_url = 'http://localhost:8000/';
-  url = proxy_url + url;
+  // url = proxy_url + url;
 
-
-  //sets playing to true so channel buttons can work
+  // sets playing to true so channel buttons can work
   playing = true;
 
-
-  //unmutes player and sets source
+  // Unmutes player and sets source
   player.muted = false;
   var hls = new Hls();
   hls.loadSource(url);
   hls.attachMedia(player);
-    
   playPromise = player.play();
 
-  //unhides player and makes it full screen
+  // Unhides player and makes it full screen
   player.requestFullscreen();
-  document.getElementById('curPlaying').style.display='';
-
-
+  document.getElementById('curPlaying').style.display = '';
 }
 
-//loads channels and their respective buttons
-function loadChannels(page){
+// Loads channels and their respective buttons
+function loadChannels(page) {
+  // Loads channel from local storage
+  iChannels = JSON.parse(localStorage.getItem('iChannels'));
 
-  //loads channel from local storage
-  iChannels = JSON.parse(localStorage.getItem("iChannels"));
-
-  if (iChannels != null){
-
-    //creates button for each channel
+  if (iChannels != null) {
+    // Creates button for each channel
     iChannels.forEach((channel, i) => {
-
-
-      if(i % 12 == 0 && i != 0){
-        console.log(pgCounter)
+      if (i % 12 == 0 && i != 0) {
+        console.log(pgCounter);
         pgCounter++;
-      
       }
 
-
-      if(pgCounter == page){
-       
-        
+      if (pgCounter == page) {
         console.log(channel.pgNmbr);
-        const ch = document.createElement("button");
-      
+        const ch = document.createElement('button');
+
         ch.title = channel.title;
-        ch.innerHTML = "<img src=\""+channel.logoUrl+"\" alt=\""+channel.title+"\">"
-      
+        ch.innerHTML = "<img src=\"" + channel.logoUrl + "\" alt=\"" + channel.title + "\">";
         ch.tabIndex = i + 1;
-        ch.lang = localStorage.getItem("lang");
-
+        ch.lang = localStorage.getItem('lang');
         ch.className = 'channelButton';
-
-        ch.onclick = function () { playC(channel.url, i); };
+        ch.onclick = function() {
+          playC(channel.url, i);
+        };
         document.getElementById('channelWrapper').appendChild(ch);
-
       }
-      
-      
-      
     });
-
   }
+}
        
   
 
-    
-    
-
-};
-
-
-
-
-//when user exits fullscreen close video
+// when user exits fullscreen close video
 document.addEventListener("fullscreenchange", function() {
   if (!document.fullscreen) {
-  
-    //plays sound effect and makes video element invisible
+    // plays sound effect and makes video element invisible
     backChannel.play();
-    document.getElementById('curPlaying').style.display='none';
+    document.getElementById('curPlaying').style.display = 'none';
     playing = false;
 
     try {
-
       if (playPromise !== undefined) {
-        playPromise.then(_ => {})
-        .catch(error => {
-        });
+        playPromise.then(_ => {}).catch(error => {});
       }
-  
-      //mutes player and unmutes background music, destroys player 
+      // mutes player and unmutes background music, destroys player
       muteChange();
       player.muted = 'true';
       hls.destroy();
-      
-  
-       
     } catch (error) {
       console.error(error);
-    
     }
-
-  } 
+  }
 });
 
 
-//controlls channel buttons
-  document.removeEventListener('keydown', keys);
-  var keys = function(e){
-    console.log(e);
-  
-    console.log(curIndex);
-    //console.log(iChannels.length);
-    
-    if (e.key == 'd' && (pgCounter > 0) && (page < pgCounter)){
-      pgCounter = 0;
+// controlls channel buttons
+document.removeEventListener('keydown', keys);
+var keys = function(e) {
+  console.log(e);
+  console.log(curIndex);
 
-      okSetting.play();
+  if (e.key == 'd' && (pgCounter > 0) && (page < pgCounter) && playing == false) {
+    pgCounter = 0;
+    okSetting.play();
 
-      //makes video element invisible
-      document.getElementById('channelWrapper').innerHTML = "";
-      page++;
-      loadChannels(page);
-    }
-    else if (e.key == 'a' && page > 0){
-      okSetting.play();
-      pgCounter = 0;
+    // makes video element invisible
+    document.getElementById('channelWrapper').innerHTML = "";
+    page++;
+    loadChannels(page);
+  } else if (e.key == 'a' && page > 0 && playing == false) {
+    okSetting.play();
+    pgCounter = 0;
 
-
-      //makes video element invisible
-      document.getElementById('channelWrapper').innerHTML = "";
-      page--;
-      loadChannels(page);
-    }
-    //plays next channel in playlist if user clicks n
-    else if (e.key == 'n' && iChannels != null && playing == true && curIndex + 1 <= iChannels.length){
-      playC(iChannels[curIndex + 1].url, ++curIndex);
-    }
-    //plays prev channel in playlist if user clicks p
-    else if (e.key == 'p' && iChannels != null && playing == true && curIndex - 1 >= 0){
-      playC(iChannels[curIndex - 1].url, --curIndex);
-    }
-    //plays random channel in playlist if user clicks r
-    else if (e.key == 'r' && iChannels != null && playing == true){
-      var rand = Math.floor(Math.random() * iChannels.length);
-      playC(iChannels[rand].url, rand);
-    }
-    
+    // makes video element invisible
+    document.getElementById('channelWrapper').innerHTML = "";
+    page--;
+    loadChannels(page);
+  } else if (e.key == 'n' && iChannels != null && playing == true && curIndex + 1 <= iChannels.length) {
+    // plays next channel in playlist if user clicks n
+    playC(iChannels[curIndex + 1].url, ++curIndex);
+  } else if (e.key == 'p' && iChannels != null && playing == true && curIndex - 1 >= 0) {
+    // plays prev channel in playlist if user clicks p
+    playC(iChannels[curIndex - 1].url, --curIndex);
+  } else if (e.key == 'r' && iChannels != null && playing == true) {
+    // plays random channel in playlist if user clicks r
+    var rand = Math.floor(Math.random() * iChannels.length);
+    playC(iChannels[rand].url, rand);
   }
-  document.addEventListener("keydown", keys);
+}
+document.addEventListener("keydown", keys);
